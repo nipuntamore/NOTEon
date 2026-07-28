300347551a014b6b5218c1a890aa91f6cd63b380
Nipun Tamore <nipunstamore357@gmail.com>
2026-07-29 00:50:10 +0530

    new-tasks css and improvisations

--- notes/forms.py
+++ notes/forms.py
@@ -1,8 +1,25 @@
-from django.forms import forms
+from django import forms
 from .models import notes
 
 class create_todo(forms.ModelForm):
     class Meta:
         model = notes
         fields = ['title','content','priority','attachment']
-        
+
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter note title'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Write your note here...'
+            }),
+            'priority': forms.Select(attrs={
+                'class': 'form-control'  # or 'form-select' in Bootstrap 5
+            }),
+            'attachment': forms.FileInput(attrs={
+                'class': 'form-control-file'  # or 'form-control' in Bootstrap 5
+            }),
+        }
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .forms import create_todo
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
     return render(request,"notes/index.html")
 
-#def New_Task(request):
+def New_Task(request):
+    if request.method == 'POST':
+        form = create_todo(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        form = create_todo()
+    return render(request, "notes/newtodo.html",{'form':form})
+
+    
