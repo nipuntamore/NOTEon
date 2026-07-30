b0bc7a37fb8f4934101f3fd040a0905902674567
Nipun Tamore <nipunstamore357@gmail.com>
2026-07-30 19:14:17 +0530

    added account login register

--- notes/models.py
+++ notes/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-#from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import FileExtensionValidator
 
 # Create your models here.
-"""class User(AbstractUser):
-    pass"""
+class User(AbstractUser):
+    pass
 
 class notes(models.Model):
     title = models.CharField()
--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns=[
     path("",views.index,name='index'),
-    path("New",views.New_Task,name='newtask'),
-    path("Archive",views.Archive,name='archive')
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("Archive/",views.Archive,name='archive'),
+    path("New/",views.New_Task,name='newtask'),
 ]
